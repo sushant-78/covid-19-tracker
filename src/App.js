ef74eafa996f9630ba81a2ba4a02d228e785cee0
sushant <sushantw78@gmail.com>
2022-01-15 21:17:28 +0530

    add return undefined to .find method in App.js

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ export default class App extends Component {
                 if (currentCountry.Country.toLowerCase() === selectedCountry) {
                     return currentCountry;
                 }
+                return undefined;
             });
 
             if (!countryData) {
